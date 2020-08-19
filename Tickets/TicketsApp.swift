1c7f025af62485439fb61366a0158b082a8ca240
mark lucking <mark.lucking@gmail.com>
2020-08-19 10:33:27 +0200

    Initial Commit

---
+++ Tickets/TicketsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicketsApp.swift
+//  Tickets
+//
+//  Created by localadmin on 19.08.20.
+//
+
+import SwiftUI
+
+@main
+struct TicketsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
